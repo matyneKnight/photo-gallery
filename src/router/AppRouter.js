75441a7e30d0d670a6aaf7cc7597922f5265882c
Abdel Matyne LAWANI <matyne.lawani@bennu.tech>
2021-05-04 10:54:09 +0000

    Updated react app

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from '../components/Header';
 import HomePage from '../components/HomePage';
-import Gallary from '../components/Gallary';
+import Gallery from '../components/Gallery';
 import NotFoundPage from '../components/NotFoundPage';
 
 const AppRouter = () => (
@@ -12,7 +12,7 @@ const AppRouter = () => (
       <div className="main-content">
         <Switch>
           <Route component={HomePage} path="/" exact={true} />
-          <Route component={Gallary} path="/gallery" />
+          <Route component={Gallery} path="/gallery" />
           <Route component={NotFoundPage} />
         </Switch>
       </div>
