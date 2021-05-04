eed73c7eb08fda73e7583692f142ee493e25aefe
Abdel Matyne LAWANI <matyne.lawani@bennu.tech>
2021-05-04 10:54:25 +0000

    Add node api server

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const photosRouter = require('./routers/photos');
+require('./db/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3300;
+
+// app.use(cors());
+app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(photosRouter);
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+ });
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
